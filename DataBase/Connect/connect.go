3dab5ab8e69314b022107f8e5eb5279eea2fabd3
Geng812 <39304725+Geng812@users.noreply.github.com>
2019-07-21 19:54:39 +0800

    Create connect.go

---
+++ DataBase/Connect/connect.go
@@ -0,0 +1,16 @@
+package connect
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+func InitDB() *gorm.DB {
+  db, err := gorm.Open("mysql", "account:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+  
+  if err != nil {
+    panic("failed to connect database!!")
+  }
+  
+  return db
+}
