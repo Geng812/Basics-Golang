4ae888d069f668a3194c371721fb9c1d2be74ffd
Geng812 <39304725+Geng812@users.noreply.github.com>
2020-01-03 02:08:23 +0800

    Update model.go

--- DataBase/Model/model.go
+++ DataBase/Model/model.go
@@ -1,9 +1,8 @@
-package model
 
 import (
-  _ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-type Test struct {
-  Name string `json:"name"`
+type Tests struct {
+	Name string `json:"name"`
 }
