5d6e36bf2dae84cefbfc837808b627424bbf4435
Geng812 <39304725+Geng812@users.noreply.github.com>
2020-01-07 00:43:04 +0800

    Update main.go

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import(
   "Web/router"
+  "DataBase/connect"
 )
 
 func main() {
-  r := router.Init()
-  r.Run()
+  db := connect.Data
+	defer db.Close()
+
+	var porgs []model.Tests
+	db.First(&porgs)
+	fmt.Print(porgs)
 }
